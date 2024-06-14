9b90fd89cc1bce38e1cff55b28d659a5787db552
Kishlaya Kesarwani <66960784+Kishlaya1999@users.noreply.github.com>
2024-06-14 14:00:08 +0000

    Rendering SignUp, Login or Logout conditionally

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,36 @@ import { Link } from "react-router-dom";
 // styles
 import styles from "./Navbar.module.css";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-	const {logout} = useLogout();
+	const { logout } = useLogout();
+	const { user } = useAuthContext();
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li className={styles.title}>myMoney</li>
 
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/signup">Sign up</Link>
-				</li>
+				{user ?
+					<>
+						<span>Hi, {user.displayName}</span>
+						<li>
+							<button className="btn" onClick={logout}>Log Out</button>
+						</li>
+					</>
+					:
+					<>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/signup">Sign up</Link>
+						</li>
+					</>
+				}
 
-				<li>
-					<button className="btn" onClick={logout}>Log Out</button>
-				</li>
+
+
 			</ul>
 		</nav>
 	);
