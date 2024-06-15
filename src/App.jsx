2507a4334a77e1f4862f7e2164998b2af8fce7f2
Kishlaya1999 <kishlayacs9935996006@gmail.com>
2024-06-16 00:19:10 +0530

    onAuthStateChanged and Route Guarding (Redirect) functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 /** @format */
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // pages & components
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/SignUp";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+	const { user, authIsReady } = useAuthContext();
+	console.log(user, authIsReady);
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Navbar />
-				<Switch>
-					<Route exact path="/">
-						<Home />
-					</Route>
-					<Route path="/login">
-						<Login />
-					</Route>
-					<Route path="/signup">
-						<Signup />
-					</Route>
-				</Switch>
-			</BrowserRouter>
+			{authIsReady && (
+				<BrowserRouter>
+					<Navbar />
+					<Switch>
+						{/* If the application doesn't get "user" object from firebase then we redirect user to signup page and user can't access home page without signup */}
+						{/* If we get "user" object from firebase then we redirect user to Home and user can't access signup or login page*/}
+						<Route exact path="/">
+							{user ? <Home /> : <Redirect to="/signup" />}
+						</Route>
+						<Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+						<Route path="/signup">
+							{user ? <Redirect to="/" /> : <Signup />}
+						</Route>
+					</Switch>
+				</BrowserRouter>
+			)}
 		</div>
 	);
 }
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,28 +1,47 @@
-import { createContext, useReducer } from "react";
+/** @format */
+
+import { createContext, useEffect, useReducer } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
-    switch(action.type){
-        case "LOGIN": 
-            return {...state, user: action.payload};
-        case "LOGOUT": 
-            return {...state, user: null};
-        default: 
-            return state;
-    }
-}
+	switch (action.type) {
+		case "LOGIN":
+			return { ...state, user: action.payload };
+		case "LOGOUT":
+			return { ...state, user: null };
+		case "AUTH_IS_READY":
+			return { ...state, user: action.payload, authIsReady: true };
+		default:
+			return state;
+	}
+};
 
-export const AuthContextPorvider = ({children}) => {
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null
-    });
+export const AuthContextPorvider = ({ children }) => {
+	const [state, dispatch] = useReducer(authReducer, {
+		user: null,
+		authIsReady: false,
+	});
 
-    console.log("AuthContext State", state);
+	// useReducer(reducerFunction, initialState)
 
-    return (
-        <AuthContext.Provider value={{...state, dispatch}}>
-            {children}
-        </AuthContext.Provider>
-    );
+	// Lesson 129 : Udemy
+	useEffect(() => {
+		// This function is going to run when we initially enter into our application. Based on our current authentication state i.e logged in or not . We would be redirected to the respective pages
+		// Logged In ==> Home
+		// Logged out ==> SignUp or Login Page
+		// We only need to check the auth status initially
+		projectAuth.onAuthStateChanged((user) => {
+			// user logged in => user obj
+			// user logged out => null
+			// But whenever we run login or logout "AUTH_IS_READY" is going to dispatched we only want to dispatch it once in very beginning, for that firebase gives us as unsubscribe function which we need to invoke it for stopping it to run
+			const unsub = dispatch({ type: "AUTH_IS_READY", payload: user });
+			unsub();
+		});
+	}, []);
+
+	console.log("AuthContext State", state);
+
+	return <AuthContext.Provider value={{ ...state, dispatch }}>{children}</AuthContext.Provider>;
 };
