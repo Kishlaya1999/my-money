4f60b59a422b800bbb730e68301605825a7c28d2
Kishlaya1999 <kishlayacs9935996006@gmail.com>
2024-06-19 00:21:40 +0530

    Transaction form created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 /** @format */
 
-import React from "react";
-import "./Home.module.css";
+// styles
+import styles from "./Home.module.css";
 
-const Home = () => {
-	return <div>Home</div>;
-};
+// components
+import TransactionForm from "./TransactionForm";
 
-export default Home;
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>transaction list</div>
+			<div className={styles.sidebar}>
+				<TransactionForm />
+			</div>
+		</div>
+	);
+}
